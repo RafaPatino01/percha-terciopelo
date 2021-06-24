04629b63989ddca4bda83b6ee728622ed0788393
opinon1 <opinon1@users.noreply.github.com>
2021-06-24 12:56:51 -0500

    added scroll to stories

--- src/js/stories.js
+++ src/js/stories.js
@@ -1,6 +1,6 @@
 
 /*
-    Carousel
+   mecanismo carousel
 */
 $('#carousel-example').on('slide.bs.carousel', function (e) {
     /*
@@ -24,3 +24,54 @@ $('#carousel-example').on('slide.bs.carousel', function (e) {
         }
     }
 });
+
+
+/*
+   mecanismo scroll
+*/
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+
+
+        function debounce(func, wait, immediate) {
+            var timeout;
+            return function() {
+                var context = this,
+                    args = arguments;
+                var later = function() {
+                    timeout = null;
+                    if (!immediate) func.apply(context, args);
+                };
+                var callNow = immediate && !timeout;
+                clearTimeout(timeout);
+                timeout = setTimeout(later, wait);
+                if (callNow) func.apply(context, args);
+            };
+        }
+
+        var slider = document.getElementById("stories");
+        var onScroll = debounce(function(direction) {
+            console.log(direction);
+            if (direction == false) {
+                $('.carousel-control-next').click();
+            } else {
+                $('.carousel-control-prev').click();
+            }
+        }, 100, true);
+
+        slider.addEventListener("wheel", function(e) {
+            e.preventDefault();
+            var delta;
+            if (event.wheelDelta) {
+                delta = event.wheelDelta;
+            } else {
+                delta = -1 * event.deltaY;
+            }
+
+            onScroll(delta >= 0);
+        });
+
+
+    }
+};
+
