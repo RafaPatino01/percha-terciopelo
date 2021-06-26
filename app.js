a7e32562a333568d50814553a99abd64c3d1c58c
Rafa Patino <RafaPatino01@outlook.com>
2021-06-26 18:48:34 -0500

    Fixed error database conection

--- app.js
+++ app.js
@@ -83,16 +83,16 @@ function handleDisconnect() {
 
   connection.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
+      console.log('[ERR] error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }
     else {
-    	console.log("Database server running...");
+    	console.log("[OK] Database server running...");
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
   connection.on('error', function(err) {
-    console.log('db error', err);
+    console.log('[ERR] DB disconection...');
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
       handleDisconnect();                         // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
