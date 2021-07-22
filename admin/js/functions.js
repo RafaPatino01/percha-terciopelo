842c727d59bfa7387b5348a06e862d32abe9e5e7
opinon1 <opinon1@users.noreply.github.com>
2021-07-22 08:20:37 -0500

    added feat delete

--- admin/js/functions.js
+++ admin/js/functions.js
@@ -304,6 +304,33 @@ async function loadPosts(p_page) {
 				document.getElementById("output").innerHTML += "No hay interviews";
 			}
 
+			// feat
+			if (JSON.stringify(feat) != no_result){
+				document.getElementById("output").innerHTML += "<h1 class='display-4 mb-3'>Feat</h1>";
+				for(var i = 0; i < feat.length; i++)
+				{
+					document.getElementById("output").innerHTML += `
+					<div class="row" id="FadeIn">
+						<div class="col-md-1 bg-lighter5 text-right">
+						<a class="btn" href="/admin_delete" onclick="disable('feat',`+ feat[i].id +`);">
+						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+							<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+							<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+						</svg>
+						</a>
+						</div>
+						<div class="col-md-11 bg-secondary">
+						<h3>`+ feat[i].title +`</h3>
+						</div>
+					</div><br>
+					`;
+				}
+			}
+			else {
+				document.getElementById("output").innerHTML += "No hay feat";
+			}
+
+
 			break;
 
 		default:
@@ -320,15 +347,23 @@ async function disable(p_section, p_id) {
 		case "post":
 			var url = "/delete_post/"+p_id;
 			var response = await fetch(url, { method: 'PUT' });
+			break;
 		case "col":
 			var url = "/delete_col/"+p_id;
 			var response = await fetch(url, { method: 'PUT' });
+			break;
 		case "news":
 			var url = "/delete_news/"+p_id;
 			var response = await fetch(url, { method: 'PUT' });
+			break;
 		case "interview":
 			var url = "/delete_interview/"+p_id;
 			var response = await fetch(url, { method: 'PUT' });
+			break;
+		case "feat":
+			var url = "/delete_feat/"+p_id;
+			var response = await fetch(url, { method: 'PUT' });
+			break;
 	}
 }
 
