13f284fa314526bc6e1e04b58a7b6b69f4b297d2
RamonRuizPe <rualejandro.ra@gmail.com>
2022-01-20 00:58:52 -0600

    Dior live is now working

--- app.js
+++ app.js
@@ -1034,7 +1034,7 @@ app.get('/admin_js/:filename', function(req, res) {
 // [ PUBLIC Routing ] ----------------------------------------------------------------------
 
 // Send live ToryBurch
-app.get('/toryburch', function(req, res) {
+app.get('/live', function(req, res) {
 	var mobile = req.useragent.isMobile;
 	if(mobile){res.sendFile(path.join(__dirname + '/src/tory.html'));} //mobile
 	if(!mobile){res.sendFile(path.join(__dirname + '/src/tory.html'));} //desktop
