ed274ee1263cb9d0abf911bae439dc7b8671c4fb
RamonRuizPe <rualejandro.ra@gmail.com>
2022-02-28 18:06:21 -0600

    Dior live page on

--- app.js
+++ app.js
@@ -1033,8 +1033,8 @@ app.get('/admin_js/:filename', function(req, res) {
 
 // [ PUBLIC Routing ] ----------------------------------------------------------------------
 
-// Send live ToryBurch
-app.get('/live', function(req, res) {
+// Send live 
+app.get('/dioraw22', function(req, res) {
 	var mobile = req.useragent.isMobile;
 	if(mobile){res.sendFile(path.join(__dirname + '/src/tory.html'));} //mobile
 	if(!mobile){res.sendFile(path.join(__dirname + '/src/tory.html'));} //desktop
